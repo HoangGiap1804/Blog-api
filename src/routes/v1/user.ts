75467f98bd9b50e5aa53d23813fd32044e366a7c
Nguyen Giap <hgiap1804@gmail.com>
2025-08-26 14:47:23 +0700

    feat(auth): implement update infomation and delete user account endpoint

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -8,6 +8,8 @@ import { body, param, query } from 'express-validator';
  * Controller
  */
 import getCurrentUser from '@/controllers/v1/user/get_current_user';
+import updateCurrentUser from '@/controllers/v1/user/update_current_user';
+import deleteCurrentUser from '@/controllers/v1/user/delete_current_user';
 
 /**
  * Middlewares
@@ -23,11 +25,72 @@ import User from '@/models/user';
 
 const router = Router();
 
+// Get current user
 router.get(
   '/current',
   authenticate,
   authorize(['admin', 'user']),
   getCurrentUser,
+);
+
+// Update information current user
+router.put(
+  '/current',
+  authenticate,
+  authorize(['admin', 'user']),
+  body('username')
+    .optional()
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage('Username must be less than 20 characters')
+    .custom(async (value) => {
+      const usernameExists = await User.exists({ username: value });
+
+      if (usernameExists) {
+        throw new Error('This username is already in use');
+      }
+    }),
+  body('email')
+    .optional()
+    .isLength({ max: 50 })
+    .withMessage('Email must be less than 50 characters')
+    .isEmail()
+    .withMessage('Invalid email address')
+    .custom(async (value) => {
+      const emailExists = await User.exists({ email: value });
+
+      if (emailExists) {
+        throw new Error('This email is already in use');
+      }
+    }),
+  body('password')
+    .optional()
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters'),
+  body('first_name')
+    .optional()
+    .isLength({ max: 20 })
+    .withMessage('First name must be less than 20 characters'),
+  body('first_name')
+    .optional()
+    .isLength({ max: 20 })
+    .withMessage('Last name must be less than 20 characters'),
+  body(['website', 'facebook', 'instagram', 'x', 'youtube'])
+    .optional()
+    .isURL()
+    .withMessage('Invalid URL')
+    .isLength({ max: 100 })
+    .withMessage('URL must be less than 100 characters'),
+  validationError,
+  updateCurrentUser,
+);
+
+// Delete current user account
+router.delete(
+  '/current',
+  authenticate,
+  authorize(['admin', 'user']),
+  deleteCurrentUser,
 );
 
 export default router;
